d3c569d057771f7862acd3bcfbdcfbddbe7f83ce
jamie <capistrano.jamiee@gmail.com>
2020-10-01 16:00:10 +0800

    changes

--- Employee.php
+++ Employee.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+ASDASDASDASD
 class Employee extends CI_Controller {
 	public function __construct(){
 		parent::__construct(); 
